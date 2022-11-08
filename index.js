43f9d85d48514f0182f852be0b86c324d06dd0ad
asifiqbal07 <as.asif.07@gmail.com>
2022-11-08 19:34:17 +0600

    data updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express()
@@ -23,6 +23,14 @@ async function run() {
             const packages = await cursor.toArray();
             res.send(packages);
         })
+
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const package = await packageCollection.findOne(query);
+            res.send(package);
+        });
+
 
     }
     finally {
